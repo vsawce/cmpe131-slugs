404870d8b9e6204e1bcca74e0d02915123f304ef
koyuul <shannonkyle.deleon@gmail.com>
2022-11-30 23:44:29 -0800

    weather data will now only send day of, and also adding transit data

--- gather.py
+++ gather.py
@@ -1,31 +1,46 @@
 import requests
 import json
 import datetime
-api_url = "https://api.openweathermap.org/data/3.0/onecall?lat=37.335480&lon=-121.893028&appid=eb584b1f51c58eeaf838fba4e2997f46&exclude=current,minutely,hourly,alerts"
+import pprint
+general_data = {}
+# Get weather forecast for next week.
+# forecast  returns object that includes: temperature, weather, timestamp, weekday
+WEATHER_URL = "https://api.openweathermap.org/data/3.0/onecall?lat=37.335480&lon=-121.893028&appid=eb584b1f51c58eeaf838fba4e2997f46&exclude=current,minutely,hourly,alerts"
 WEEKDAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 weather_params = {
     "units": "imperial"
 }
 
 weather_data = requests.get(
-    api_url,
+    WEATHER_URL,
     params=weather_params
 );
 
 weather = json.loads(weather_data.content)
-weather_str = json.dumps(weather, indent=4)
 
-forecast = [];
-for day in weather["daily"]:
-    dt = datetime.datetime.fromtimestamp(day["dt"])
-    timestamp = dt.strftime("%m/%d/%Y, %H:%M:%S")
-    weekday = WEEKDAYS[dt.weekday()]
-    day_data = {
-        "temperature": day["temp"]["day"],
-        "weather": day["weather"][0]["main"],
-        "datetime_stamp": timestamp,
-        "weekday": weekday
-    }
-    forecast.append(day_data);
+day = weather["daily"][0]
+dt = datetime.datetime.fromtimestamp(day["dt"])
+timestamp = dt.strftime("%m/%d/%Y, %H:%M:%S")
+weekday = WEEKDAYS[dt.weekday()]
+day_data = {
+    "temperature": day["temp"]["day"],
+    "condition": day["weather"][0]["main"],
+}
+general_data["weather"] = day_data
 
+# Get nearby transit stop leaving times
+# Returns array of nearby transit stops as objects, including their route #'s, and leaving times
+TRANSIT_URL = "https://transit.hereapi.com/v8/departures?ids=401902823,718301021&maxPerBoard=1&apiKey=c6TZkG4BVXmMloUipnKq8GepDwFL2eNaGLzvywjeAPU"
+transit_response = requests.get(TRANSIT_URL)
+upcoming_stops = json.loads(transit_response.content)
+transit_data = []
+for board in upcoming_stops["boards"]:
+    time_unformatted= board["departures"][0]["time"][0:-6]
+    time = datetime.datetime.strptime(time_unformatted, '%Y-%m-%dT%H:%M:%S')
+    difference_in_seconds = (time - datetime.datetime.now()).total_seconds()
+    difference_in_minutes = divmod(difference_in_seconds, 60)
+    display_minutes = ("99+", str(difference_in_minutes[0])[:-2]) [difference_in_minutes[0] < 99]
+    transit_data.append(board["departures"][0]["transport"]["shortName"] + ": " + display_minutes)
+general_data["transit"] = transit_data
 
+print(json.dumps(general_data, indent=4))
