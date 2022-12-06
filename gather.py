40fe483279342fea696c2407486d7fedb40db457
koyuul <shannonkyle.deleon@gmail.com>
2022-12-06 00:31:53 -0800

    added printing messages for demo

--- gather.py
+++ gather.py
@@ -1,11 +1,12 @@
 import requests
 import json
 import datetime
-import pprint
-general_data = {}
+import random
+
+general_data = []
 # Get weather forecast for next week.
 # forecast  returns object that includes: temperature, weather, timestamp, weekday
-WEATHER_URL = "https://api.openweathermap.org/data/3.0/onecall?lat=37.335480&lon=-121.893028&appid=eb584b1f51c58eeaf838fba4e2997f46&exclude=current,minutely,hourly,alerts"
+WEATHER_URL = "https://api.openweathermap.org/data/3.0/onecall?lat=37.335480&lon=-121.893028&appid=eb584b1f51c58eeaf838fba4e2997f46&exclude=minutely,hourly,daily,alerts"
 WEEKDAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 weather_params = {
     "units": "imperial"
@@ -17,16 +18,8 @@ weather_data = requests.get(
 );
 
 weather = json.loads(weather_data.content)
-
-day = weather["daily"][0]
-dt = datetime.datetime.fromtimestamp(day["dt"])
-timestamp = dt.strftime("%m/%d/%Y, %H:%M:%S")
-weekday = WEEKDAYS[dt.weekday()]
-day_data = {
-    "temperature": day["temp"]["day"],
-    "condition": day["weather"][0]["main"],
-}
-general_data["weather"] = day_data
+general_data.append(str(weather["current"]["feels_like"])+"F");
+general_data.append(weather["current"]["weather"][0]["main"]);
 
 # Get nearby transit stop leaving times
 # Returns array of nearby transit stops as objects, including their route #'s, and leaving times
@@ -40,7 +33,19 @@ for board in upcoming_stops["boards"]:
     difference_in_seconds = (time - datetime.datetime.now()).total_seconds()
     difference_in_minutes = divmod(difference_in_seconds, 60)
     display_minutes = ("99+", str(difference_in_minutes[0])[:-2]) [difference_in_minutes[0] < 99]
-    transit_data.append(board["departures"][0]["transport"]["shortName"] + ": " + display_minutes)
-general_data["transit"] = transit_data
+    general_data.append(board["departures"][0]["transport"]["shortName"] + ":" + display_minutes)
 
-print(json.dumps(general_data, indent=4))
+# Get food of the day
+# Will eventually update to datacrawl
+dc_menu = ["C.KATSU", "BBQ CHKN", "BEEF STW", "PORK BUN"]
+dc_item = dc_menu[random.randint(0, 3)]
+general_data.append(dc_item)
+
+# Get parking garage info
+general_data.append("N:99%")
+general_data.append("S:85%")
+general_data.append("SW:79%")
+
+data = "|".join(general_data)
+req = requests.put("https://cmpe-led-server.herokuapp.com/update/?data=" + data)
+print(req + data)
