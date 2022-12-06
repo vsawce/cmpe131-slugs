aa196aacf09f4bcaac940fffe56699ce9320ddfe
vsawce <vsaw30@gmail.com>
2022-12-06 00:36:58 -0800

    V1: Made adjustments to Serial invokations to relay info to the Uno

--- arduino-esp8266/campus-display-wifi/campus-display-wifi.ino
+++ arduino-esp8266/campus-display-wifi/campus-display-wifi.ino
@@ -23,18 +23,20 @@ ESP8266WiFiMulti WiFiMulti;
 
 void setup() {
 
-  Serial.begin(115200);
+  Serial.begin(9600);
   // Serial.setDebugOutput(true);
 
-  Serial.println();
-  Serial.println();
-  Serial.println();
+  //Serial.println();
+  //Serial.println();
+  //Serial.println();
 
-  for (uint8_t t = 4; t > 0; t--) {
-    Serial.printf("[SETUP] WAIT %d...\n", t);
-    Serial.flush();
-    delay(1000);
-  }
+  //for (uint8_t t = 4; t > 0; t--) {
+  //  Serial.printf("[SETUP] WAIT %d...\n", t);
+  //  Serial.flush();
+  //  delay(1000);
+  //}
+
+  delay(4000); //4s
 
   WiFi.mode(WIFI_STA);
   WiFiMulti.addAP(SSID, PASSWORD);
@@ -52,17 +54,17 @@ void loop() {
 
     HTTPClient https;
 
-    Serial.print("[HTTPS] begin...\n");
+    //Serial.print("[HTTPS] begin...\n");
     if (https.begin(*client, "https://cmpe-led-server.herokuapp.com/")) {  // HTTPS
 
-      Serial.print("[HTTPS] GET...\n");
+      //Serial.print("[HTTPS] GET...\n");
       // start connection and send HTTP header
       int httpCode = https.GET();
 
       // httpCode will be negative on error
       if (httpCode > 0) {
         // HTTP header has been send and Server response header has been handled
-        Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+        //Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
 
         // file found at server
         if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
@@ -70,16 +72,16 @@ void loop() {
           Serial.println(payload);
         }
       } else {
-        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+        //Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
       }
 
       https.end();
     } else {
-      Serial.printf("[HTTPS] Unable to connect\n");
+      //Serial.printf("[HTTPS] Unable to connect\n");
     }
   }
 
-  Serial.println("Wait 10s before next round...");
+  //Serial.println("Wait 10s before next round...");
   delay(10000);
 }
 
